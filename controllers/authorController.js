2f947846f05f58508784b1d4b9450295ea7e7048
zu3x <zu3x@fresnou.org>
2023-03-07 11:33:28 -0800

    sixth, Authors, problem with adding

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,4 @@
-const {Author, Book} = require('../models')
+const {Author} = require('../models')
 module.exports.viewAll = async function (req, res) {
     const authors = await Author.findAll();
     res.render('author/view_all', {authors});
@@ -6,4 +6,47 @@ module.exports.viewAll = async function (req, res) {
 module.exports.viewProfile = async function(req,res){
     const author = await Author.findByPk(req.params.id);
     res.render('author/profile', {author})
+}
+module.exports.renderEditForm = async function(req, res){
+    const author = await Author.findByPk(req.params.id);
+    res.render('author/edit', {author});
+}
+module.exports.updateAuthor = async function(req, res){
+    const author = await Author.update({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        books_written: req.body.books_written,
+        dob: req.body.dob
+    }, {
+        where: {
+            id: req.params.id
+        }
+    });
+    res.redirect(`/authors/profile/${req.params.id}`);
+}
+module.exports.renderAddForm = function(req, res){
+    const author = {
+        first_name: '',
+        last_name: '',
+        books_written: '',
+        dob: '12-12-2000',
+    }
+    res.render('author/add', {author});
+}
+module.exports.addAuthor = async function(req,res){
+    const author = await Author.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        books_written: req.body.books_written,
+        dob: req.body.dob,
+    });
+    res.redirect(`/authors/profile/${author.id}`);
+}
+module.exports.deleteAuthor = async function(req,res){
+    await Author.destroy({
+        where:{
+            id:req.params.id
+        }
+    });
+    res.redirect('/authors');
 }
